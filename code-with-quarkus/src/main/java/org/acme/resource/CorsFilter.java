faa2243cbcf9978ed56d11e0a5b0cd989fc937b3
henriqued11234 <henrique.dpastor@sp.senac.br>
2025-10-19 20:36:44 -0300

    Update CorsFilter.java

--- code-with-quarkus/src/main/java/org/acme/resource/CorsFilter.java
+++ code-with-quarkus/src/main/java/org/acme/resource/CorsFilter.java
@@ -1,4 +1,4 @@
-package org.acme.resource;
+package com.yourcompany.agoravai.config;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
@@ -12,13 +12,18 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, 
                       ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add(
-            "Access-Control-Allow-Origin", "https://henriqued11234.github.io");
-        responseContext.getHeaders().add(
-            "Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add(
-            "Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add(
-            "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        
+        String origin = requestContext.getHeaderString("Origin");
+        String allowedOrigins = "https://henriqued11234.github.io,https://abcd-ndjr.onrender.com";
+        
+        if (origin != null && allowedOrigins.contains(origin)) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+        } else {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://henriqued11234.github.io");
+        }
+        
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
